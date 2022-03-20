af89917513fcee93ab131ba0a2fb2c1727b4b266
Okan Serdaroglu <okanserdaroglu2@gmail.com>
2022-03-20 15:44:36 +0300

    dto models, ui models, mappers and api interface

---
+++ app/src/main/java/com/example/cyrptocurrencyapp/data/remote/CoinPaprikaAPI.kt
@@ -0,0 +1,19 @@
+package com.example.cyrptocurrencyapp.data.remote
+
+import com.example.cyrptocurrencyapp.data.remote.dto.CoinDetailDto
+import com.example.cyrptocurrencyapp.data.remote.dto.CoinDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface CoinPaprikaAPI {
+
+    @GET("/v1/coins")
+    suspend fun getCoins(): List<CoinDto>
+
+    @GET("/v1/coins/{coinId}")
+    suspend fun getCoinById(
+        @Path("coinId") coinId: String
+    ): CoinDetailDto
+
+}
