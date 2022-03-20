0a76be1ce276318fad76d19c90bb224e71daa508
Okan Serdaroglu <okanserdaroglu2@gmail.com>
2022-03-20 20:38:42 +0300

    CoinListScreen

---
+++ app/src/main/java/com/example/cyrptocurrencyapp/presentation/coin_list/components/CoinListItem.kt
@@ -0,0 +1,62 @@
+package com.example.cyrptocurrencyapp.presentation.coin_list.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cyrptocurrencyapp.domain.model.Coin
+
+@Composable
+fun CoinListItem(
+    coin: Coin,
+    onItemClick: (Coin) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick(coin) }
+            .padding(20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "${coin.rank}. ${coin.name} (${coin.symbol})",
+            style = MaterialTheme.typography.body1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.align(CenterVertically)
+        )
+
+        Text(
+            text = if (coin.isActive) "active" else "inactive",
+            color = if (coin.isActive) Color.Green else Color.Red,
+            fontStyle = FontStyle.Italic,
+            style = MaterialTheme.typography.body2,
+            modifier = Modifier.align(CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CoinListItemPreview() {
+    CoinListItem(
+        Coin(
+            "1",
+            true,
+            "okan",
+            5,
+            "symbol"
+        )
+    ) {}
+}
+
